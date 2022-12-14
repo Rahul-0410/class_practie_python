0f0d802ec5a71c5f678ae0a7b1ee819077493e38
Rahul <115411672+Rahul-0410@users.noreply.github.com>
2022-12-14 09:09:19 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,7 +1,8 @@
 # Find the maximum element present in a list. 
 l=list(map(int,input().split(" ")))
-min_num=min(l)
-count=l.count(min)
-for i in range(count):
-    l.remove(min_num)
-print(max(l))
+largest_num=None
+for number in l:
+    if largest_num is None or largest_num< number:
+        largest_num=number
+
+print(largest_num)
